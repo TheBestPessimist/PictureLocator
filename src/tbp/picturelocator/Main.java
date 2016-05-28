3ae02eeb8bb1a94f761e575b83f9abe37e8abfb3
TheBestPessimist <cristian.nahsuc@gmail.com>
2016-05-28 11:17:04 +0300

    Remove old comments

--- src/tbp/picturelocator/Main.java
+++ src/tbp/picturelocator/Main.java
@@ -17,26 +17,19 @@ import pt.karambola.gpx.parser.GpxParserOptions;
 
 public class Main {
 
-public static final Path TEST_FILE_RELATIVE_PATH = Paths.get("resources/gpx/st_petersburg_May_1,_2016_09_49_41.gpx").toAbsolutePath();
-    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
-        Gpx workingGPX;
+  public static final Path TEST_FILE_RELATIVE_PATH = Paths.get("resources/gpx/st_petersburg_May_1,_2016_09_49_41.gpx").toAbsolutePath();
 
-        try(BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(TEST_FILE_RELATIVE_PATH))){
-          GpxParserOptions gpxParserOptions = GpxParserOptions.PARSE_ALL;
-          gpxParserOptions.setDateFormat( new SimpleDateFormat( "yyyy-MM-dd'T'hh:mm:ss.SSS'Z'" ));
-          workingGPX = new GpxParser().parseGpx(bis, gpxParserOptions);
-        }
-//      System.out.println(workingGPX.getPoints());
-//      System.out.println(workingGPX.getRoutes());
-//      System.out.println(workingGPX.getTracks());
-//      System.out.println(workingGPX.getExtensionsParsed());
-      for (Track t : workingGPX.getTracks()){
-//        System.out.println("t" + t);
-//        System.out.println("t.getComment()" + t.getComment());
-//        System.out.println("t.getDescription()" + t.getDescription());
-//        System.out.println("t.getName()" + t.getName());
-        System.out.println("t.getTrackPoints()" + t.getTrackPoints());
-        t.getTrackPoints().forEach(System.out::println);
-      }
+  public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+    Gpx workingGPX;
+
+    try (BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(TEST_FILE_RELATIVE_PATH))) {
+      GpxParserOptions gpxParserOptions = GpxParserOptions.PARSE_ALL;
+      gpxParserOptions.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'"));
+      workingGPX = new GpxParser().parseGpx(bis, gpxParserOptions);
     }
+    for (Track t : workingGPX.getTracks()) {
+      System.out.println("t.getTrackPoints()" + t.getTrackPoints());
+      t.getTrackPoints().forEach(System.out::println);
+    }
+  }
 }
